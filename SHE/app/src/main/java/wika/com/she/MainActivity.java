0aaefa713c3806eb1cdfa9ae9a004f96656f464f
Adi Prasetyo <adiprasetyo27@gmail.com>
2017-02-21 20:15:01 +0700

    Add logo on Action Bar

--- SHE/app/src/main/java/wika/com/she/MainActivity.java
+++ SHE/app/src/main/java/wika/com/she/MainActivity.java
@@ -1,5 +1,6 @@
 package wika.com.she;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
+
     }
 }
