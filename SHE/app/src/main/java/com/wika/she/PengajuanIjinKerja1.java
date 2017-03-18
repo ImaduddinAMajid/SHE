b7558077bd486af91ee1efb57b7e724c080ae192
Adi Prasetyo <adiprasetyo27@gmail.com>
2017-03-18 08:39:14 +0700

    add PengajuanIjinKerja1 activity

---
+++ SHE/app/src/main/java/com/wika/she/PengajuanIjinKerja1.java
@@ -0,0 +1,13 @@
+package com.wika.she;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PengajuanIjinKerja1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengajuan_ijin_kerja1);
+    }
+}
