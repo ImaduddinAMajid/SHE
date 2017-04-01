27c31998a6273979ec6bf6af17851340cbd25981
Adi Prasetyo <adiprasetyo27@gmail.com>
2017-04-01 16:28:28 +0700

    add http request form alat berat

--- SHE/app/src/main/java/com/wika/she/activity/admin/FormComissioningAlat.java
+++ SHE/app/src/main/java/com/wika/she/activity/admin/FormComissioningAlat.java
@@ -4,9 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
 import com.wika.she.R;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class FormComissioningAlat extends AppCompatActivity{
+public class FormComissioningAlat extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinner;
+    private String spinner_item;
+    private EditText editTextTanggal, editTextTipe, editTextNoIdentitas, editTextPemilik, editTextOperator, editTextCatatan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +25,48 @@ public class FormComissioningAlat extends AppCompatActivity{
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setSubtitle(R.string.sub_title);
         setContentView(R.layout.activity_form_comissioning_alat);
+
+        this.populateElement();
     }
 
-    public void next(View view) {
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        this.spinner_item = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void next(View view) throws JSONException {
+        JSONObject dataJson = new JSONObject();
+        dataJson.put("jenisAlat", this.spinner_item);
+        dataJson.put("tanggal", this.editTextTanggal.getText().toString());
+        dataJson.put("tipe", this.editTextTipe.getText().toString());
+        dataJson.put("noIdentitas", this.editTextNoIdentitas.getText().toString());
+        dataJson.put("pemilik", this.editTextPemilik.getText().toString());
+        dataJson.put("operator", this.editTextOperator.getText().toString());
+        dataJson.put("catatan", this.editTextCatatan.getText().toString());
+
         Intent comissionIntent2 = new Intent(this, FormComissioningAlat2.class);
+        comissionIntent2.putExtra("data", dataJson.toString());
         startActivity(comissionIntent2);
     }
 
     public void cancel(View view) {
 
+    }
+
+    public void populateElement() {
+        this.spinner = (Spinner)findViewById(R.id.spinner_alat);
+        this.spinner.setOnItemSelectedListener(this);
+        this.editTextTanggal = (EditText)findViewById(R.id.edit_text_tanggal);
+        this.editTextTipe = (EditText)findViewById(R.id.edit_text_tipe);
+        this.editTextNoIdentitas = (EditText)findViewById(R.id.edit_text_no_identitas);
+        this.editTextPemilik = (EditText)findViewById(R.id.edit_text_pemilik);
+        this.editTextOperator = (EditText)findViewById(R.id.edit_text_operator);
+        this.editTextCatatan = (EditText)findViewById(R.id.edit_text_catatan);
     }
 }
 
