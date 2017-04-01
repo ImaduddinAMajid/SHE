9d6d360bcaff37734779d79777a0acd81663d497
Adi Prasetyo <adiprasetyo27@gmail.com>
2017-04-01 19:48:39 +0700

    add she induction activity

---
+++ SHE/app/src/main/java/com/wika/she/activity/admin/SheIduction.java
@@ -0,0 +1,52 @@
+package com.wika.she.activity.admin;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import com.wika.she.R;
+
+public class SheIduction extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setSubtitle(R.string.sub_title);
+        setContentView(R.layout.activity_she_iduction);
+
+        this.imageButton = (ImageButton)findViewById(R.id.button_image_dokumentasi);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            this.imageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void photoClicked(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void cancel(View view) {
+
+    }
+
+    public void submit(View view) {
+
+    }
+}
