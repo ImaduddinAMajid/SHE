bdc589ad3ed9a1e17f4e5db6f9363837a764ac36
ImaduddinAMajid <imad@accelbyte.net>
2017-04-09 17:19:38 +0700

    add POST, GET, PUT method

--- server/serverPDO.php
+++ server/serverPDO.php
@@ -9,8 +9,151 @@ $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 switch($operation){
 
+    case induction:
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $pekerjaId = $obj -> pekerjaId;
+        $namaPekerja = $obj -> namaPekerja;
+        $tanggal = $obj -> tanggal;
+        $telepon = $obj -> telepon;
+        $jabatan = $obj -> jabatan;
+        $unitKerja = $obj -> unitKerja;
+        $noKTP = $obj -> noKTP;
+        $fotoPeserta = $obj -> fotoPeserta;
+
+        if(!empty($namaPekerja)){
+            $sql = "INSERT INTO Pekerja (pekerjaId, namaPekerja, tanggal, telepon, jabatan, unitKerja, noKTP, fotoPeserta) 
+VALUES (?,?,?,?,?,?,?,?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($pekerjaId, $namaPekerja, $tanggal, $telepon, $jabatan, $unitKerja, $noKTP, $fotoPeserta));
+            $data = '{"status":"success"}';
+        }
+
+        else{
+            $data = '{"status":"failed"}';
+        }
+
+        echo $data;
+
+        break;
+
+    case insiden:
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $namaKorban = $obj->namaKorban;
+        $TTLKorban = $obj->TTLKorban;
+        $jabatan=$obj->jabatan;
+        $unitKerja=$obj->unitKerja;
+        $tipeInsiden=$obj->tipeInsiden;
+        $kronologi=$obj->kronologi;
+        $saksiKejadian=$obj->saksiKejadian;
+        $waktuKejadian=$obj->waktuKejadian;
+        $akibatInsiden=$obj->akibatInsiden;
+        $estimasiKerugian=$obj->estimasiKerugian;
+        $dokumentasi=$obj->dokumentasi;
+
+        if(!empty($namaKorban)){
+            $sql = "INSERT INTO Insiden (namaKorban, TTLKorban, jabatan, unitKerja, tipeInsiden, kronologi, saksiKejadian, waktuKejadian, akibatInsiden, estimasiKerugian, dokumentasi)
+VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($namaKorban,$TTLKorban,$jabatan, $unitKerja, $tipeInsiden, $kronologi, $saksiKejadian, $waktuKejadian, $akibatInsiden, $estimasiKerugian, $dokumentasi));
+            $data = '{"status":"success"}';
+        }
+
+        else $data = '{"status":"success"}';
+
+        echo $data;
+
+
+        break;
+
+    case list_pegawai_ijin:
+        /*Source code for showing list of Pekerja*/
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $unitKerja = $obj->unitKerja;
+
+        if(!empty($unitKerja)){
+            $sql = "SELECT noKTP, namaPekerja, jabatan FROM Pekerja WHERE unitKerja = ? ";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($unitKerja));
+            $data = json_encode($q->fetchAll(PDO::FETCH_ASSOC));
+        }
+        else $data = '{"status":"failed"}';
+
+        echo $data;
+
+        break;
+
+    case persetujuan_ijin:
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $pengaju = $obj->pengaju;
+        $tanggal = $obj->tanggal;
+
+        if(!empty($pengaju)){
+            $sql = "UPDATE Ijin_Kerja SET persetujuan = '1' WHERE pengaju = ? AND tanggal = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($pengaju,$tanggal));
+            $data = '{"status":"success"}';
+        }
+        else $data = '{"status":"failed"}';
+
+        echo $data;
+
+        break;
+
+    case pengajuan_ijin:
+        /*Source code for inserting data to Ijin_Kerja table*/
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $pengaju =                  $obj->pengaju;
+        $tanggal =                  $obj->tanggal;
+        $lokasiKerja =              $obj->lokasiKerja;
+        $lingkupKerja =             $obj->lingkupKerja;
+        $jamKerja =                 $obj->jamKerja;
+        $kebutuhanAlatBerat =       $obj->kebutuhanAlatBerat;
+        $pekerjaTerlibat =          $obj->pekerjaTerlibat;
+        $kelengkapanKeselamatan =   $obj->kelengkapanKeselamatan;
+        $perlengkapanKerja =        $obj->perlengkapanKerja;
+        $izinKerjaKhusus =          $obj->izinKerjaKhusus;
+        $catatan =                  $obj->catatan;
+        $persetujuan =              $obj->persetujuan;
+
+        if(!empty($pengaju)){
+            $sql = "INSERT INTO Ijin_Kerja (pengaju, tanggal, lokasiKerja, lingkupKerja, jamKerja, kebutuhanAlatBerat, pekerjaTerlibat, kelengkapanKeselamatan, persetujuan)
+ VALUES(?,?,?,?,?,?,?,?,?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($pengaju, $tanggal, $lokasiKerja,$lingkupKerja,$jamKerja,$kebutuhanAlatBerat,$pekerjaTerlibat,$kelengkapanKeselamatan,$persetujuan));
+            $data = '{"status":"success"}';
+
+        }
+
+        else $data = '{"status":"failed"}';
+
+        echo $data;
+
+        break;
+
+    case list_pengajuan_ijin:
+        /*Source code for showing data of Ijin_Kerja table*/
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $pengaju = $obj->pengaju;
+        $tanggal = $obj->tanggal;
+
+        if(!empty($pengaju)){
+            $sql = "SELECT * FROM Ijin_Kerja WHERE  pengaju = ? AND tanggal = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($pengaju,$tanggal));
+            $data = json_encode($q->fetchAll(PDO::FETCH_ASSOC));
+            echo $data;
+        }
+
+        break;
+
     case alat_berat:
-        /*Pengisian data ke database Alat Berat*/
+        /*Source code for inserting data to Alat_Berat table*/
         $json = file_get_contents('php://input');
         $obj = json_decode($json);
         $jenisAlat = $obj->jenisAlat;
@@ -25,11 +168,16 @@ switch($operation){
         $fotoKanan = $obj->fotoKanan;
         $fotoKiri = $obj->fotoKiri;
 
-        $sql = "INSERT INTO Alat_Berat (jenisAlat, tanggal, tipe, noIdentitas, pemilik, operator, catatan, fotoDepan, fotoBelakang, fotoKanan, fotoKiri) 
+        if(!empty($jenisAlat)){
+            $sql = "INSERT INTO Alat_Berat (jenisAlat, tanggal, tipe, noIdentitas, pemilik, operator, catatan, fotoDepan, fotoBelakang, fotoKanan, fotoKiri) 
 VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-        $q = $pdo->prepare($sql);
-        $q->execute(array($jenisAlat,$tanggal,$tipe,$noIdentitas,$pemilik,$operator,$catatan,$fotoDepan,$fotoBelakang,$fotoKanan,$fotoKiri));
-        $data = '{"status":"success"}';
+            $q = $pdo->prepare($sql);
+            $q->execute(array($jenisAlat,$tanggal,$tipe,$noIdentitas,$pemilik,$operator,$catatan,$fotoDepan,$fotoBelakang,$fotoKanan,$fotoKiri));
+            $data = '{"status":"success"}';
+        }
+
+        else $data = '{"status":"failed"}';
+
         echo $data;
 
         break;
@@ -59,6 +207,8 @@ VALUES(?,?,?,?,?,?,?,?,?,?,?)";
             echo $data;
 
         }
+
+        else $data = '{"status":"failed"}';
 
         break;
 
